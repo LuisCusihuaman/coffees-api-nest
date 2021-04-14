0d3f8ea2fe37f98b5b635f21f8165da2ad9666fb
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-04-14 15:05:15 -0300

    feat(coffees): create coffee

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
+  @Get('')
   findAll() {
     return 'This action return all coffees';
   }
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This action return #${id} coffee`;
+  }
+  @Post()
+  create(@Body() body) {
+    return body;
   }
 }
